66dba3d0e68a50a79be430afb62cd8d56cd02014
0xATHERIS <ath3ris@proton.me>
2023-07-12 01:08:43 +0100

    feat: :sparkles:  - example of the hook in use

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,23 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import PlaceholderImage from './assets/placeholder.png';
+import { useZoomImage } from '../.';
 
 const App = () => {
+  const [showImage, setShowImage] = React.useState(false);
+
+  const [ref, ExpandedImage] = useZoomImage(showImage, { backdrop: true });
+
   return (
     <div>
-      <Thing />
+      <h1>Examples to use useZoomImage on:</h1>
+
+      <br />
+
+      <img ref={ref} src={PlaceholderImage} alt="Placeholder Image" onClick={() => setShowImage(true)} />
+      {showImage && <ExpandedImage backdropOnClick={() => setShowImage(false)} />}
+
     </div>
   );
 };
